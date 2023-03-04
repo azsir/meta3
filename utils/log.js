2b7d999383e7e1be7e3c68c66aeef89d5cd1e9b4
AZsir <123969802+azsir@users.noreply.github.com>
2023-03-04 16:49:04 +0600

    Update of 3/23

--- utils/log.js
+++ utils/log.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.bold.hex("#FF0000").bold('[ Error ] » ') + data);
+			console.log(chalk.bold.hex("#FF0000").bold('[ Warning ] » ') + data);
 			break;
 		case "error":
 			console.log(chalk.bold.hex("#FF0000").bold('[ Error ] »') + data);
@@ -19,19 +19,19 @@ job[Math.floor(Math.random() * job.length)]
 module.exports.loader = (data, option) => {
 	switch (option) {   
 		case "success":
-			console.log(chalk.bold.hex("#00FF00").bold('[ Daniel ] ❯ ') + data);
+			console.log(chalk.bold.hex("#00FF00").bold('[ Command on ] ❯ ') + data);
 			break;
 		case "warn":
-			console.log(chalk.bold.hex("#FF0000").bold('[ Daniel ] ❯ ') + data);
+			console.log(chalk.bold.hex("#FF0000").bold('[ Warning at ] ❯ ') + data);
 			break;
 		case "error":
-			console.log(chalk.bold.hex("#FF0000").bold('[ Daniel ] ❯ ') + data);
+			console.log(chalk.bold.hex("#FF0000").bold('[ Error ] ❯ ') + data);
 			break;
 		default:
       var job = ["#FFA500","#FFFF00","#00FF00","#0000FF", "#4B0082","#A020F0"];
     var random = 
 job[Math.floor(Math.random() * job.length)]
-			console.log(chalk.bold.hex(random).bold(`[ Daniel ] ❯ `) + data);
+			console.log(chalk.bold.hex(random).bold(`[ Floring ] ❯ `) + data);
 			break;
 	}
 	}
