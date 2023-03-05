ce7b7188b6286aa1049c170593f10da96b6a1477
AZsir <123969802+azsir@users.noreply.github.com>
2023-03-05 14:25:49 +0600

    Update antiout.js

--- script/events/antiout.js
+++ script/events/antiout.js
@@ -1,22 +1,37 @@
 module.exports.config = {
- name: "antiout",
- eventType: ["log:unsubscribe"],
- version: "0.0.1",
- credits: "DungUwU",
- description: "Listen events"
-};
-
-module.exports.run = async({ event, api, Threads, Users }) => {
- let data = (await Threads.getData(event.threadID)).data || {};
- if (data.antiout == false) return;
- if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
- const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
- const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
- if (type == "self-separation") {
-  api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
-   if (error) {
-    api.sendMessage(`Unable to re-add members ${name} to the group :( `, event.threadID)
-   } else api.sendMessage(`Active antiout mode, ${name} has been re-added to the group!`, event.threadID);
-  })
- }
-}
+    name: "🚫 Anti-Out",
+    eventType: ["log:unsubscribe"],
+    version: "1.0.0",
+    credits: "🥀 Zihad",
+    description: "Prevents members from leaving the group"
+  };
+  
+  module.exports.run = async ({ event, api, Threads, Users }) => {
+    let data = (await Threads.getData(event.threadID)).data || {};
+    if (!data.antiout) return;
+  
+    if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+  
+    const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+    const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
+  
+    if (type == "self-separation") {
+      api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
+        if (error) {
+          api.sendMessage(`❌ Unable to re-add member ${name} to the group.`, event.threadID)
+        } else {
+          const gifUrl = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDUyYjdmYjNjOTM5OTgxZmIyN2RmZjg4MmNhNjQ1Yzk4YjFmM2Q3YSZjdD1n/X3zJUXUgaCTNPLZ1wJ/giphy.gif";
+          api.sendMessage({
+            body: `🚫 Anti-out mode is active! ${name} has been re-added to the group.`,
+            attachment: {
+              type: "animated_image",
+              payload: {
+                url: gifUrl
+              }
+            }
+          }, event.threadID);
+        }
+      })
+    }
+  }
+  
