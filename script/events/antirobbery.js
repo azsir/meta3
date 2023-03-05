8c062466651709f55ba7ab49f58dd94abd488817
AZsir <123969802+azsir@users.noreply.github.com>
2023-03-05 14:26:55 +0600

    Update antirobbery.js

--- script/events/antirobbery.js
+++ script/events/antirobbery.js
@@ -1,77 +1,43 @@
 module.exports.config = {
-	name: "joinNoti",
-	eventType: ["log:subscribe"],
-	version: "1.0.1",
-	credits: "Zihad - 🥀", 
-	description: "📢 Notify bot or group member with random gif/photo/video",
-	dependencies: {
-		"fs-extra": "📁",
-		"path": "🛣️",
-		"pidusage": "📊"
-	}
+  name: "guard",
+  eventType: ["log:thread-admins"],
+  version: "1.0.0",
+  credits: "zihad - 🥀",
+  description: "🔒 Prevents changes to admin roles in a group chat.",
 };
 
-module.exports.onLoad = function () {
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { join } = global.nodemodule["path"];
-
-	const path = join(__dirname, "cache", "joinGif");
-	if (existsSync(path)) mkdirSync(path, { recursive: true });	
-
-	const path2 = join(__dirname, "cache", "joinGif", "randomgif");
-    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
-
-    return;
-}
-
-
-module.exports.run = async function({ api, event }) {
-	const { join } = global.nodemodule["path"];
-	const { threadID } = event;
-	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-		api.changeNickname(`{ ${global.config.PREFIX} } × ${(!global.config.BOTNAME) ? "bot" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
-		const fs = require("fs");
-		return api.sendMessage("", event.threadID, () => api.sendMessage({body:`🤖 META BOT CONNECTED!!! \n\n📋 MENU CMD : ${global.config.PREFIX}help\n\n\n✅ Contact\n- Facebook : ${global.config.DEVURL}
-\n\n✅ Thanks For Using Our FREE Package❤️\n\n 🔰 For BUY PREMIUM 💎💎 Contact The Admin 🤖`, attachment: fs.createReadStream(__dirname + "/cache/joinmp4/hello.gif")} ,threadID));
-	}
-	else {
-		try {
-			const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
-			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
-
-			const threadData = global.data.threadData.get(parseInt(threadID)) || {};
-			const path = join(__dirname, "cache", "joinGif");
-			const pathGif = join(path, `${threadID}.gif`);
-
-			var mentions = [], nameArray = [], memLength = [], i = 0;
-			
-			for (id in event.logMessageData.addedParticipants) {
-				const userName = event.logMessageData.addedParticipants[id].fullName;
-				nameArray.push(userName);
-				mentions.push({ tag: userName, id });
-				memLength.push(participantIDs.length - i++);
-			}
-			memLength.sort((a, b) => a - b);
-			
-			(typeof threadData.customJoin == "undefined") ? msg = "👋 Welcome {name}. to {threadName}\n{type} are the {soThanhVien}th member of this group.\n\n" : msg = threadData.customJoin;
-			msg = msg
-			.replace(/\{name}/g, nameArray.join(', '))
-			.replace(/\{type}/g, (memLength.length > 1) ?  'You' : 'Friend')
-			.replace(/\{soThanhVien}/g, memLength.join(', '))
-			.replace(/\{threadName}/g, threadName);
-
-			if (existsSync(path)) mkdirSync(path, { recursive: true });
-
-			const randomPath = readdirSync(join(__dirname, "cache", "joinGif", "randomgif"));
-
-			if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
-			else if (randomPath.length != 0) {
-				const pathRandom = join(__dirname, "cache", "joinGif", "randomgif", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
-				formPush = { body: msg, attachment: createReadStream(pathRandom), mentions }
-			}
-			else formPush = { body: msg, mentions }
-
-			return api.sendMessage(formPush, threadID);
-		} catch (e) { return console.log(e) };
-	}
+module.exports.run = async function ({ event, api, Threads, Users }) {
+  const { logMessageType, logMessageData, senderID } = event;
+  let data = (await Threads.getData(event.threadID)).data;
+  if (data.guard == false) return;
+  if (data.guard == true) {
+    switch (logMessageType) {
+      case "log:thread-admins": {
+        if (logMessageData.ADMIN_EVENT == "add_admin") {
+          if(event.author == api.getCurrentUserID()) return;
+          if(logMessageData.TARGET_ID == api.getCurrentUserID()) return;
+          else {
+            api.changeAdminStatus(event.threadID, event.author, false, editAdminsCallback);
+            api.changeAdminStatus(event.threadID, logMessageData.TARGET_ID, false);
+            function editAdminsCallback(err) {
+              if (err) return api.sendMessage("❌ Error occurred while changing admin status.", event.threadID, event.messageID);
+              return api.sendMessage("🚫 Your group has enabled anti-robbery feature. Please turn it off before adding new admin.", event.threadID, event.messageID);
+            }
+          }
+        }
+        else if (logMessageData.ADMIN_EVENT == "remove_admin") {
+          if(event.author == api.getCurrentUserID()) return;
+          if(logMessageData.TARGET_ID == api.getCurrentUserID()) return;
+          else {
+            api.changeAdminStatus(event.threadID, event.author, false, editAdminsCallback);
+            api.changeAdminStatus(event.threadID, logMessageData.TARGET_ID, true);
+            function editAdminsCallback(err) {
+              if (err) return api.sendMessage("❌ Error occurred while changing admin status.", event.threadID, event.messageID);
+              return api.sendMessage("🚫 Your group has enabled anti-robbery feature. Please turn it off before removing an admin.", event.threadID, event.messageID);
+            }
+          }
+        }
+      }
+    }
   }
+}
