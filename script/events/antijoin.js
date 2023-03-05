9f5aa0c14645aa00118c336a85e83981504f7cab
AZsir <123969802+azsir@users.noreply.github.com>
2023-03-05 14:26:17 +0600

    Update antijoin.js

--- script/events/antijoin.js
+++ script/events/antijoin.js
@@ -1,25 +1,32 @@
 module.exports.config = {
- name: "antijoin",
- eventType: ["log:subscribe"],
- version: "1.0.0",
- credits: "D-Jukie",
- description: "Cấm thành viên mới vào nhóm"
-};
-
-module.exports.run = async function ({ event, api, Threads, Users }) {
- 	let data = (await Threads.getData(event.threadID)).data
- 	if (data.newMember == false) return;
- 	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) return
-    else if(data.newMember == true) {
-    var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId)
-			for (let idUser of memJoin) {
-					await new Promise(resolve => setTimeout(resolve, 1000));
-					api.removeUserFromGroup(idUser, event.threadID, async function (err) {
-                        if (err) return data["newMember"] = false;
-                            await Threads.setData(event.threadID, { data });
-                              global.data.threadData.set(event.threadID, data);
-                    })
-			}
- 	return api.sendMessage(`» Your team now has Anti Join mode turned on, please turn it off before adding a new 👻 member`, event.threadID);
- }
-}
+	name: "🚫 Anti-Join",
+	eventType: ["log:subscribe"],
+	version: "1.0.0",
+	credits: "🥀 Zihad",
+	description: "Prevents new members from joining the group"
+  };
+  
+  module.exports.run = async function ({ event, api, Threads, Users }) {
+	let data = (await Threads.getData(event.threadID)).data;
+	if (!data.newMember) return;
+	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) return;
+  
+	else if (data.newMember) {
+	  var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId);
+  
+	  for (let idUser of memJoin) {
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		api.removeUserFromGroup(idUser, event.threadID, async function (err) {
+		  if (err) {
+			return data["newMember"] = false;
+		  } else {
+			await Threads.setData(event.threadID, { data });
+			global.data.threadData.set(event.threadID, data);
+		  }
+		});
+	  }
+  
+	  return api.sendMessage(`🚫 Anti-join mode is now enabled. Please disable it before adding new members.`, event.threadID);
+	}
+  }
+  
